9bc35398422f457f3fa1ee9295de7e9ef59060f6
Matt <matthave@gmail.com>
2020-03-22 01:15:33 +0100

    Updates

---
+++ precache-manifest.bf087b8aa0539760bcb5a365e4f6c634.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0813c4b346039e8d5edda591d6f48445",
+    "url": "/slider/index.html"
+  },
+  {
+    "revision": "e2acb9a0438512757065",
+    "url": "/slider/static/css/main.caf5b70a.chunk.css"
+  },
+  {
+    "revision": "127d6d4e8cfadb8b756e",
+    "url": "/slider/static/js/2.38a40735.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/slider/static/js/2.38a40735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2acb9a0438512757065",
+    "url": "/slider/static/js/main.e399da20.chunk.js"
+  },
+  {
+    "revision": "3faf51cf3cd4e46557cd",
+    "url": "/slider/static/js/runtime-main.1a38c2df.js"
+  }
+]);
